34797de04fe7a31504b131cfeecff3e5b35ecd68
dualax <158518020+dualax@users.noreply.github.com>
2025-04-15 10:28:07 +0300

    Add files via upload

---
+++ Core/DdModelContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1SqlServer.Model;
+
+namespace Task1SqlServer.Core
+{
+    public static class DdModelContext
+    {
+        public static Task1DB DB { get; set;  }
+    }
+}
+
